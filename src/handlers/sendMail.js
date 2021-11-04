dff0c2d9e8e1232873be9f8b3fb9303d6ca7013c
Gabriel Rocha <knuckles4x@gmail.com>
2021-11-04 10:50:40 -0300

    sqs config

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -3,19 +3,25 @@ import AWS from "aws-sdk";
 const ses = new AWS.SES({ region: "eu-west-1" });
 
 async function sendMail(event, context) {
+  const record = event.Records[0];
+  console.log("record processing", record);
+
+  const email = JSON.parse(record.body);
+  const { subject, body, recipient } = email;
+
   const params = {
     Source: "gabrielrocha1997@gmail.com",
     Destination: {
-      ToAddresses: ["knuckles4x@gmail.com"],
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
         Text: {
-          Data: "Hello from Blindas",
+          Data: body,
         },
       },
       Subject: {
-        Data: "Test Mail",
+        Data: subject,
       },
     },
   };
