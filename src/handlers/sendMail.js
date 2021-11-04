4b728ee54e511ad23d3f1eeeeda2c1b53dc2dd36
Gabriel Rocha <knuckles4x@gmail.com>
2021-11-04 10:17:13 -0300

    sendMail test

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,6 +1,32 @@
+import AWS from "aws-sdk";
+
+const ses = new AWS.SES({ region: "eu-west-1" });
+
 async function sendMail(event, context) {
-  console.log(event);
-  return event;
+  const params = {
+    Source: "gabrielrocha1997@gmail.com",
+    Destination: {
+      ToAddresses: ["knuckles4x@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: "Hello from Blindas",
+        },
+      },
+      Subject: {
+        Data: "Test Mail",
+      },
+    },
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export const handler = sendMail;
